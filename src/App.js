fb9cbc75b0e9dca41e8bb2a37ddf3c617f80d6d2
Reynier Tellez <reynier.tellez@gmail.com>
2018-05-09 14:52:47 +0100

    Was added the correct code to show the markers in the map

--- src/App.js
+++ src/App.js
@@ -5,10 +5,44 @@ export class MapContainer extends Component {
 	constructor(props) {
 		super(props);
 
-		const style = {
+		this.style = {
 			width: '100%',
 			height: '100%'
 		}
+		
+		this.objMarkers = [
+			{
+				title: "Malta International Airport.",
+				name: "International Airport",
+				position: ["35.85142254", "14.49219836"]
+			},
+			{
+				title: "Hagar Kin Template.",
+				name: "Hagar Kin Template",
+				position: ["35.82908772", "14.44275989"]
+			},
+			{
+				title: "Malta Free Port.",
+				name: "",
+				position: ["35.81583", "14.53768409"]
+			},
+			{
+				title: "Popeye Village.",
+				name: "Popeye",
+				position: ["35.96113379", "14.34150067"]
+			},
+			{
+				title: "Café Del Mar Malta.",
+				name: "Soma",
+				position: ["35.95905948", "14.42386195"]
+			}
+			,
+			{
+				title: "St. Paul's Catacombs.",
+				name: "St. Paul's Catacombs",
+				position: ["35.88117638", "14.39832799"]
+			}
+		]
 	}
 
 	render() {
@@ -18,7 +52,14 @@ export class MapContainer extends Component {
 				style={this.style}
 				initialCenter={{lat: 35.85472104, lng: 14.48779873}}
 				zoom={11}>
-				<Marker onClick={this.onMarkerClick} name={'Current location'} />
+
+				{this.objMarkers.map((marker, index) => (
+					<Marker
+						key={index}
+						title={marker.title}
+						name={marker.name}
+						position={{lat: marker.position[0], lng: marker.position[1]}} />
+				))}
 			</Map>
 		);
 	}
@@ -32,11 +73,3 @@ export default GoogleApiWrapper({
   apiKey: ("AIzaSyBHq-A0EL3usDeqH5q8B635Rafxcguc0a8"),
   LoadingContainer: LoadingContainer
 })(MapContainer)
-
-/*
-<InfoWindow onClose={this.onInfoWindowClose}>
-	<div>
-		<h1>{this.state.selectedPlace.name}</h1>
-	</div>
-	</InfoWindow>
-*/
