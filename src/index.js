336c08a09970c71054951877517c1d0dd16d9d26
Reynier Tellez <reynier.tellez@gmail.com>
2018-05-22 13:45:05 +0100

    Deleted unnecessary files and use only the default react service worker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/popper.js/dist/popper.min.js';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-//import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
+registerServiceWorker();
