6426c2c6b1f33a53b030e5917134dc9352023c70
Reynier Tellez <reynier.tellez@gmail.com>
2018-05-24 19:39:49 +0100

    Delete unnecessary import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './css/App.css';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-//import '../node_modules/popper.js/dist/popper.js';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import registerServiceWorker from './registerServiceWorker';
 
