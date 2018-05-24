e2704288100eb795d7fd62146c478d27ccf9e671
Reynier Tellez <reynier.tellez@gmail.com>
2018-05-24 09:37:54 +0100

    Fix popper.min.js import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './css/App.css';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/popper.js/dist/popper.min.js';
+import '../node_modules/popper.js/dist/popper.js';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import registerServiceWorker from './registerServiceWorker';
 
